2c937919b99e328930cba6ef7a1801b905096f5f
idk <51036884+AcidicSoil@users.noreply.github.com>
2021-11-17 20:42:28 -0600

    Update getHPpartnumber4.py

--- getHPpartnumber4.py
+++ getHPpartnumber4.py
@@ -21,62 +21,50 @@ from dataclasses import make_dataclass
 page = requests.get('https://partsurfer.hp.com/Search.aspx')
 page.raise_for_status()
 
-
 # Asks user for serial number
 print('enter serial number')
 #serial_number = input()
 serial_number = "8cg7222dcd"
-#print('enter item description')
-#item = input().upper()
 
-#keywords = [item]
-#print(type(keywords))
+#Asks user to enter item 
+print('enter item description:')
+item = input().upper()
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option("excludeSwitches", ["enable-logging"])
-#options.page_load_strategy = 'eager'
-options.add_argument('headless')
-driver = webdriver.Chrome(
-    options=options, executable_path=r'C:/Users/user/mypythonscripts/chromedriver')
 
+#Runs selenium in the background
+options.add_argument('headless')
+
+#Finds path to chrome driver
+driver = webdriver.Chrome(
+    options=options, executable_path=r'C:/Users/biven/mypythonscripts/chromedriver')
+
+#Goes to URL home page
 driver.get('https://partsurfer.hp.com/Search.aspx')
-#driver.manage().window().maximize()
 wait = WebDriverWait(driver, 3)
 
+#Selects country
 select = Select(driver.find_element_by_name(
     'ctl00$BodyContentPlaceHolder$ddlCountry'))
 select.select_by_visible_text('United States')
 
+#Clicks on pop-up dialog box
 element = wait.until(EC.element_to_be_clickable(
     (By.XPATH, "// *[@id='onetrust-accept-btn-handler']"))).click()
 
-#alert = driver.switch_to.alert
-
+#Finds search box
 textElem = driver.find_element_by_name(
     "ctl00$BodyContentPlaceHolder$SearchText$TextBox1")
+
+#Inputs serial number in search box
 textElem.send_keys(serial_number)
+#Clicks submit on search box
 textElem.submit()
 searchElem = driver.find_element_by_xpath(
     "//*[@id='ctl00_BodyContentPlaceHolder_SearchText_btnSubmit']").click()
-###
-#soup = bs4.BeautifulSoup(driver.page_source, features="lxml")
-#tables = soup.find_all(
-#    'table', {"id": ["ctl00_BodyContentPlaceHolder_gridSpareBOM"]})
-###
-#print(type(tables))
-
-#rows = soup.find_all("td", "title")
-###
-#table = tables[0]
-#tab_data = [[cell.text for cell in row.find_all(["th", "td"])]
-#            for row in table.find_all("tr")]
-###
-
 # Grabs part page current url and turns it into html
-
-
 url2 = driver.current_url
-#print(url2)
 
 req1 = requests.get(url2)
 if req1.status_code in [200]:
@@ -84,42 +72,14 @@ if req1.status_code in [200]:
 else:
     print('Could not retrieve: % s, err: % s - status code: % s' %
           (url2, req1.text, req1.status_code))
-#dfs = pd.read_html(url2)
-#print(type(dfs))
-#print(dfs)
-#def get_all_tabs(URL = url2):
-#    driver = webdriver.Chrome()
-#    driver.get(URL)
-#    print(url2)
-#    soup = bs4.BeautifulSoup(driver.page_source, 'lxml')
 
 driver.implicitly_wait(30)
 driver.get(url2)
-df = pd.read_html(driver.find_element_by_id("ctl00_BodyContentPlaceHolder_gridSpareBOM").get_attribute('outerHTML'))[0]
+df = pd.read_html(driver.find_element_by_id(
+    "ctl00_BodyContentPlaceHolder_gridSpareBOM").get_attribute('outerHTML'))[0]
 display(df[['Spare Part Number', 'Spare Part Description']])
-df = df[df['Spare Part Description'].str.contains('CABLE, ODD')]
-#df1 = pd.read_html(driver.find_element_by_id("ctl00_BodyContentPlaceHolder_gridSpareBOM").get_attribute('outerHTML'))[0]
-display(df['Spare Part Number'][3])
-#display(df)
-
-#soup = bs4.BeautifulSoup(driver.page_source, features="lxml")    
-#tables = soup.find_all('table', {"id": ["ctl00_BodyContentPlaceHolder_gridSpareBOM"]})
-#tabs_dic = {}
-    
-#for table in tables:
-#    tab_name = table['id']
-#    tab_data = [[cell.text for cell in row.find_all(["th", "td"])]
-#                        for row in table.find_all("tr")]
-#    df = pd.DataFrame(tab_data)
-#    df.columns = df.iloc[0, :]
-#    df.drop(index=0, inplace=True)
-
-#    tabs_dic[tab_name] = df
-#print(type(df))
-#print(type(display(df.to_string())))
-#dfs1 = pd.read_html(url2, attrs={"class": "table_sortable  tbl"})
-#print(type(dfs1))
-#print(dfs1)
+df = df[df['Spare Part Description'].str.contains(item)]
+display(df)
 sleep(2)
 #https://stackoverflow.com/questions/23580726/python-beautifulsoup-list-index-after-the-table
 #https://www.crummy.com/software/BeautifulSoup/bs4/doc/
