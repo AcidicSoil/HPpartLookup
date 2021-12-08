d24d40c7ca4dff1c0ad6f62bdeab7b92d6e1ebf9
idk <51036884+AcidicSoil@users.noreply.github.com>
2021-12-07 18:07:35 -0600

    Create site_choice.py

---
+++ site_choice.py
@@ -0,0 +1,32 @@
+#site = input("what site to search? ")
+#choice_1 = (bool(input("press 1 for ebay or 2 for amazon: ")))
+
+
+def site_choice(n):
+    if n == 1:
+        site = "ebay"
+    elif n == 2:
+        site = "amazon"
+    return site
+site_choice(input('enter 1 or 2'))
+#print(selector1)
+    #selector = int(input('enter 1 or 2: '))
+#if selector == 1:
+#    site = "ebay"
+#elif selector == 2:
+#    site = "amazon"
+#print(site_choice())
+#choice = input('choose one: '+ options)
+#default = 1
+#number = int(input("press 1 for ebay or 2 for amazon: "))
+#def site_choice(number):
+#    if number == 1:
+#        site = "ebay"
+#    elif number == 2:
+#        site = "amazon"
+ #       return site
+#print(site_choice(number))
+#number = input('enter site choice: ')
+#number = print('enter number')
+#input(site_choice(number))
+#print('your choice is: ' +str(site_choice(choice)))
